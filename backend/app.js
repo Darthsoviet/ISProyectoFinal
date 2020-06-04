1d3e2e54339230f783d8ea88d31406490d7eba0e
Darthsoviet <31465914+Darthsoviet@users.noreply.github.com>
2020-06-04 17:58:03 -0500

    mas dependencias

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
+let cors = require("cors")
 require("dotenv").config();
 const moongose= require("mongoose")
 
@@ -15,11 +16,17 @@ moongose.connect(URL,{useUnifiedTopology: true, useNewUrlParser: true} ).then(()
 
    })
 var app = express();
-
+app.use(cors)
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// carga las rutas estaticas de react
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+
 
 
 

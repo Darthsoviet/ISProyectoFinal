7e5196f2749bfcdee4ed5d9c9696d76a3d2521d2
gusdi <guchipedro@gmail.com>
2020-06-09 22:45:56 -0500

    Corrigo Schema de sistemaOperativo y empiezo a trabajar el DELETE

--- backend/models/sistemaOperativo.js
+++ backend/models/sistemaOperativo.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var sistemaOperativoSchema = Schema({
+  id: Number,
   nombre: String,
   arquitectura: String,
   versionKernel: String,
--- backend/routes/sistemasOperativos.js
+++ backend/routes/sistemasOperativos.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
-
 var mongoose = require('mongoose');
 var SistemaOperativo = require('../models/sistemaOperativo');
 
 
+router.delete('/sistemasOperativos',(requisito,respuesta,siguiente)=>{
+    respuesta.status(405).json({Aviso:"Necesitas especificar el 'id' del Sistema Operrativo que deseas eliminar "})
+});
+
+router.delete('/sistemasOperativos/:id',(requisito,respuesta,siguiente)=>{
+    SistemaOperativo.findOneAndDelete({id:requisito.params.id},(error,informacion)=>{
+        if (error) respuesta.status(404).json({Aviso:"No hemos encontrado el Sistema Operativo requerido"});
+        else respuesta.status(200).json({Exito:"Hemos logrado eliminar el S.O. de la lista"});
+    });
+});
 
 module.exports = router;
